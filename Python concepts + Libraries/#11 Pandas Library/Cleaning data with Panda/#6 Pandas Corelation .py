89f4f968ad870d0809ccf0f2955438c65c3b0a35
LGFF123 <ffirelovers35@gmail.com>
2026-06-28 00:48:00 +0500

    Pandas Library Finished

---
+++ Python concepts + Libraries/#11 Pandas Library/Cleaning data with Panda/#6 Pandas Corelation .py
@@ -0,0 +1,48 @@
+
+
+                                       # Pandas Corelation 
+
+'''
+A great aspect of the Pandas module is the corr() method.
+
+The corr() method calculates the relationship between each column in your data set.
+'''
+
+
+import pandas as pd 
+
+abj=pd.read_csv(r"C:\Python learning\Python concepts + Libraries\#11 Pandas Library\Cleaning data with Panda\Dataset.csv")
+
+print(abj.corr())
+
+
+'''
+
+Result Explained
+The Result of the corr() method is a table with a lot of numbers that represents how well 
+the relationship is between two columns.
+
+The number varies from -1 to 1.
+
+1 means that there is a 1 to 1 relationship (a perfect correlation), and for this data set, 
+each time a value went up in the first column, the other one went up as well.
+
+0.9 is also a good relationship, and if you increase one value, the other will probably increase 
+as well.
+
+-0.9 would be just as good relationship as 0.9, but if you increase one value, the other will 
+probably go down.
+
+0.2 means NOT a good relationship, meaning that if one value goes up does not mean that 
+the other will
+
+'''
+# In simple  words it tell how much they are related 1 is for direct propotionality and 
+# -1 shows indirect propotionality . 
+
+
+# If the corelation is e.g 0.0048  then it means this relation will not help in any  kind of 
+# prediction as both data are not much related 
+
+
+
